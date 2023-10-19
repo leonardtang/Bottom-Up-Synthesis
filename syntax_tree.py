8e391f07615ed2c063b89ca98e745f0edb35b8cb
Leonard <leonardgentwintang@gmail.com>
2023-10-18 23:43:59 -0400

    Blazing fast baby

--- syntax_tree.py
+++ syntax_tree.py
@@ -1,29 +1,35 @@
 from abc import ABC, abstractmethod
 
+
 class AbstractSyntaxTree(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, operator, left, right, child):
+        self.operators = None
+        self.operator = operator
+        self.left = left
+        self.right = right
+        self.child = child
 
     @abstractmethod
     def evaluate(self):
         pass
 
     @abstractmethod
-    def combine(self):
+    def construct(self):
         pass
+
 
 class ArithmeticSyntaxTree(AbstractSyntaxTree):
     def __init__(self, operator, left=None, right=None, child=None):
-        self.operators = ['+', 'x', '/', '-', 'input', 'identity']
+        self.operators = ["+", "x", "/", "-", "input", "identity"]
         self.operator = operator
-        self.unary_operators = ['input', 'identity']
-        self.binary_operators = ['+', 'x', '/', '-']
+        self.unary_operators = ["input", "identity"]
+        self.binary_operators = ["+", "x", "/", "-"]
         assert self.operator in self.operators
-        if operator == 'input':
+        if operator == "input":
             self.left = None
             self.right = None
             self.child = None
-        elif operator == 'identity':
+        elif operator == "identity":
             assert child is not None
             self.left = None
             self.right = None
@@ -35,64 +41,79 @@ class ArithmeticSyntaxTree(AbstractSyntaxTree):
             self.child = None
 
     def evaluate(self, input_val):
-        '''
+        """
         Calculate the numerical value of the given program
-        '''
+        """
         # print('operator', self.operator)
         # print('left', self.left)
         # print('right', self.right)
 
         # Hit UnaryExpression: retrieve single numerical value
-        if self.operator == 'input':
+        if self.operator == "input":
             return input_val
-        if self.operator == 'identity':
+        if self.operator == "identity":
             return self.child
 
         # BinaryExpressions
-        if self.operator == '+':
+        if self.operator == "+":
             return self.left.evaluate(input_val) + self.right.evaluate(input_val)
-        elif self.operator == 'x':
+        elif self.operator == "x":
             return self.left.evaluate(input_val) * self.right.evaluate(input_val)
-        elif self.operator == '/':
+        elif self.operator == "/":
             return self.left.evaluate(input_val) / self.right.evaluate(input_val)
-        elif self.operator == '-':
+        elif self.operator == "-":
             return self.left.evaluate(input_val) - self.right.evaluate(input_val)
         else:
             assert False
 
-    def combine(self):
-        '''
+    def construct(self):
+        """
         Recursively construct a program (i.e. use grammar to generate code)
-        '''
-        if self.operator == 'input':
-            return f'y'
-        if self.operator == 'identity':
-            return f'{self.child}'
-        
-        if self.operator == '+':
-            return f'({self.left.combine()} + {self.right.combine()})'
-        elif self.operator == 'x':
-            return f'({self.left.combine()} x {self.right.combine()})'
-        elif self.operator == '/':
-            return f'({self.left.combine()} / {self.right.combine()})'
-        elif self.operator == '-':
-            return f'({self.left.combine()} - {self.right.combine()})'
+        """
+        if self.operator == "input":
+            return f"y"
+        if self.operator == "identity":
+            return f"{self.child}"
+
+        if self.operator == "+":
+            return f"({self.left.construct()} + {self.right.construct()})"
+        elif self.operator == "x":
+            return f"({self.left.construct()} x {self.right.construct()})"
+        elif self.operator == "/":
+            return f"({self.left.construct()} / {self.right.construct()})"
+        elif self.operator == "-":
+            return f"({self.left.construct()} - {self.right.construct()})"
         else:
             assert False
 
+
 class StringSyntaxTree(AbstractSyntaxTree):
     def __init__(self, operator, left=None, right=None, child=None):
-        # TODO: move the common code into AbstractClass
-        self.operators = ['concat', 'right', 'left', 'input', 'input_x', 'input_y', 'identity']
-        self.unary_operators = ['right', 'left', 'input', 'input_x', 'input_y', 'identity']
-        self.binary_operators = ['concat']
+        self.operators = [
+            "concat",
+            "right",
+            "left",
+            "input",
+            "input_x",
+            "input_y",
+            "identity",
+        ]
+        self.unary_operators = [
+            "right",
+            "left",
+            "input",
+            "input_x",
+            "input_y",
+            "identity",
+        ]
+        self.binary_operators = ["concat"]
         self.operator = operator
         assert self.operator in self.operators
-        if operator in {'input', 'input_x', 'input_y'}:
+        if operator in {"input", "input_x", "input_y"}:
             self.left = None
             self.right = None
             self.child = None
-        elif operator in {'identity', 'left', 'right'}:
+        elif operator in {"identity", "left", "right"}:
             assert child is not None
             self.left = None
             self.right = None
@@ -103,56 +124,55 @@ class StringSyntaxTree(AbstractSyntaxTree):
             self.right = right
             self.child = None
 
-        # self.terminals = [StringSyntaxTree(operator='identity', )]
-
     def evaluate(self, input_val):
-        '''
+        """
         Calculate the string value of the given program
-        '''
+        """
         # Unary
-        if self.operator == 'input':
+        if self.operator == "input":
             return input_val
-        elif self.operator == 'input_x':
+        elif self.operator == "input_x":
             assert type(input_val) == list
             assert len(input_val) == 2
             return input_val[0]
-        elif self.operator == 'input_y':
+        elif self.operator == "input_y":
             assert type(input_val) == list
             assert len(input_val) == 2
             return input_val[1]
-        elif self.operator == 'identity':
+        elif self.operator == "identity":
             return self.child
-        elif self.operator == 'left':
+        elif self.operator == "left":
             return self.child.evaluate(input_val)[0]
-        elif self.operator == 'right':
+        elif self.operator == "right":
             return self.child.evaluate(input_val)[-1]
 
         # Binary
-        if self.operator == 'concat':
+        if self.operator == "concat":
             return self.left.evaluate(input_val) + self.right.evaluate(input_val)
         else:
             assert False
 
-    def combine(self):
-        '''
+    def construct(self):
+        """
         Recursively construct a program (i.e. use grammar to generate code)
-        '''        
+        """
         # Unary
-        if self.operator in ['input', 'input_x']:
-            return 'Input(x)'
-        elif self.operator == 'input_y':
-            return 'Input(y)'
-        elif self.operator == 'identity':
-            return f'{self.child}'
-        elif self.operator == 'left':
-            return f'Left({self.child.combine()})'
-        elif self.operator == 'right':
-            return f'Right({self.child.combine()})'
-        
+        if self.operator in ["input", "input_x"]:
+            return "Input(x)"
+        elif self.operator == "input_y":
+            return "Input(y)"
+        elif self.operator == "identity":
+            return f"{self.child}"
+        elif self.operator == "left":
+            return f"Left({self.child.construct()})"
+        elif self.operator == "right":
+            return f"Right({self.child.construct()})"
+
         # Binary
-        if self.operator == 'concat':
-            return f'Concat({self.left.combine()}, {self.right.combine()})'
+        if self.operator == "concat":
+            return f"Concat({self.left.construct()}, {self.right.construct()})"
         else:
             assert False
+
 
 # TODO: extend left/right to more than just most left and most right?
--- synthesizer.py
+++ synthesizer.py
@@ -1,56 +1,59 @@
-from syntax_tree import ArithmeticSyntaxTree, StringSyntaxTree
+import numpy as np
+from collections import defaultdict
 from itertools import combinations_with_replacement
+from syntax_tree import ArithmeticSyntaxTree, StringSyntaxTree
 
-# Just start off with arithmetic for now
+
 class Synthesizer:
     """
-    Input: I/O examples (I, O)
+    Input: I/O examples (I, O) and AST
     Output: Program consistent with (I, O)
     """
+
     def __init__(self, in_list, out_list, ast) -> None:
         self.inputs = in_list
         self.outputs = out_list
         self.ast_template = ast
-        # How to factor in input from the in_list???
-        # ArithmeticDSL terminals:
-        # self.terminal_nodes = [ast(operator='identity', child=0), ast(operator='identity', child=1), ast(operator='input')]
-        # StringDSL terminals
-        self.terminal_nodes = []
-        for inp in self.inputs:
-            if type(inp) is list:
-                assert len(inp) == 2
-                self.terminal_nodes.append(ast(operator='input_x', child=inp[0]))
-                self.terminal_nodes.append(ast(operator='input_y', child=inp[1]))
-            elif type(inp) is str:
-                self.terminal_nodes.append(ast(operator='input', child=inp))
-        self.terminal_nodes.append(ast(operator='identity', child=' '))
+
+        if ast is ArithmeticSyntaxTree:
+            self.terminal_nodes = [
+                ArithmeticSyntaxTree(operator="identity", child=0),
+                ArithmeticSyntaxTree(operator="identity", child=1),
+                ArithmeticSyntaxTree(operator="identity", child=5),
+                ArithmeticSyntaxTree(operator="input")
+            ]
+        elif ast is StringSyntaxTree:
+            self.terminal_nodes = []
+            for inp in self.inputs:
+                if type(inp) is list:
+                    assert len(inp) == 2
+                    self.terminal_nodes.append(ast(operator="input_x", child=inp[0]))
+                    self.terminal_nodes.append(ast(operator="input_y", child=inp[1]))
+                elif type(inp) is str:
+                    self.terminal_nodes.append(ast(operator="input", child=inp))
+            self.terminal_nodes.append(StringSyntaxTree(operator="identity", child=" "))
+        
         self.plist = self.terminal_nodes
-        # for p in self.plist:
-        #     print(p.combine())
-        #     print(p.evaluate(self.inputs[0]))
         # Horribly hacky way of getting operators
         self.operators = self.plist[-1].operators
         self.uni_ops = self.plist[-1].unary_operators
         self.bin_ops = self.plist[-1].binary_operators
 
     def grow(self):
-        # Given a list of expressions, return all 1) expressions and 2) values that can be obtained 
-        # by applying one of the operations to expressions in the list
-        # print(self.plist)
-        # input()
+        # Given a list of expressions, return all expressions that can be obtained by applying an operation
         new_plist = []
-        # Maybe need to split into binary vs. unary operators
+        # Apply unary operators
         for child_prog in self.plist:
             for op in self.uni_ops:
-                if op == 'input' or op == 'identity': continue
-                # print('unary op', op)
-                # print('unary outer child', child_prog.operator)
+                if op == "input" or op == "identity":
+                    continue
                 candidate_prog = self.ast_template(op, child=child_prog)
-                expression = candidate_prog.combine()
+                expression = candidate_prog.construct()
                 # if expression.startswith('Concat(Concat(Input(x), )'):
                 # print('unary Expression', expression)
                 new_plist.append(candidate_prog)
 
+        # Apply binary operators
         for pair in combinations_with_replacement(self.plist, 2):
             left_prog, right_prog = pair
             for op in self.bin_ops:
@@ -59,39 +62,59 @@ class Synthesizer:
                 # print('bin outer left', left_prog.operator)
                 # print('bin outer right', right_prog.operator)
                 candidate_prog = self.ast_template(op, left=left_prog, right=right_prog)
-                # expression = candidate_prog.combine()
-                # test = right_prog.combine()
+                expression = candidate_prog.construct()
+                # test = right_prog.construct()
                 # if test == 'Input(y)':
-                #     print('bin Expression', expression)
+                # print('bin Expression', expression)
                 new_plist.append(candidate_prog)
-
-        # Prune here...
+        
+        # new_plist = self.sort(new_plist)
+        # new_plist = self.prune(new_plist)
         self.plist.extend(new_plist)
 
-    def prune(self):
-        # Do something to plist. Eliminate observational equivalents
-        # Test on all inputs to see which ones are the same
-        # Seems like you would only want to do this on non-terminals
-        pass
+    def prune(self, candidates):
+        """
+        Eliminate observational equivalents from a list of candidate programs
+        """
+        outputs_db = defaultdict(list)
+        for idx, p in enumerate(candidates):
+            try:
+                outputs = [p.evaluate(self.inputs[i]) for i in range(len(self.inputs))]
+                outputs_db[tuple(outputs)].append(idx)
+            except:
+                continue
 
-    def sort(self):
-        # Sort programs by size
-        pass
+        duplicates = [idx for indices in outputs_db.values() if len(indices) > 1 for idx in indices[1:] ]
+        candidates = np.delete(candidates, duplicates).tolist()
+        return candidates
+
+    def sort(self, candidates):
+        """
+        Sort programs by parsimony (here, just by length)
+        """
+        return sorted(candidates, key=lambda p: len(p.construct()))
 
     def synthesize(self):
         while True:
             self.grow()
-            self.sort()
+            self.plist = self.sort(self.plist)
+            self.plist = self.prune(self.plist)
             for p in self.plist:
                 try:
                     # print('p.evaluate on input:', self.inputs[0])
-                    # print(p.combine())
+                    # print(p.construct())
                     # print(p.evaluate(self.inputs[0]))
-                    if all([p.evaluate(self.inputs[i]) == self.outputs[i] for i in range(len(self.inputs))]):
-                        return p.combine()
+                    if all(
+                        [
+                            p.evaluate(self.inputs[i]) == self.outputs[i]
+                            for i in range(len(self.inputs))
+                        ]
+                    ):
+                        return p.construct()
                 except:
                     # If program executes with errors, ditch it
                     continue
+
 
 def test_arithmetic():
     test_pairs = [
@@ -101,29 +124,43 @@ def test_arithmetic():
         ([1, 2, 3, 4, 5], [2, 3, 4, 5, 6]),
         ([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]),
         ([1, 2, 3, 4, 5], [-1, -2, -3, -4, -5]),
-        ([1, 2, 3, 4, 5], [-2, -4, -6, -8, -10])
+        ([1, 2, 3, 4, 5], [-2, -4, -6, -8, -10]),
+        ([3, 4, 5], [9, 16, 25]),
+        ([5, 10, 15], [1, 2, 3]),
+        ([100, 50, 20], [10, 5, 2]),
+        ([1, 2, 3], [3 * x + 5 for x in [1, 2, 3]]),
+        ([1, 2, 3], [x ** 2 + 6 * x for x in [1, 2, 3]])
     ]
     for inputs, outputs in test_pairs:
-        print('Inputs:', inputs)
-        print('Outputs:', outputs)
+        print("------------------")
+        print("Inputs:", inputs)
+        print("Outputs:", outputs)
         synth = Synthesizer(inputs, outputs, ArithmeticSyntaxTree)
         result = synth.synthesize()
-        print('Program:', result)
+        print("Program:", result)
+
 
 def test_string():
     test_pairs = [
-        (['hello', 'world'], ['h', 'w']),
-        (['hello', 'world'], ['o', 'd']),
-        ([['hello', 'you'], ['world', 'domination']], ['helloyou', 'worlddomination']),
-        ([['hello', 'you'], ['world', 'domination']], ['hello you', 'world domination']),
-        (['hello', 'world', 'domination'], ['ho', 'wd', 'dn'])
+        (["hello", "world"], ["h", "w"]),
+        (["hello", "world"], ["o", "d"]),
+        ([["hello", "you"], ["world", "domination"]], ["helloyou", "worlddomination"]),
+        (
+            [["hello", "you"], ["world", "domination"]],
+            ["hello you", "world domination"],
+        ),
+        (["hello", "world", "domination"], ["ho", "wd", "dn"]),
+        (["llms", "are", "bad"], ["ls", "ae", "bd"]),
+        ([["the", "adults"], ["are", "talking"]], ["ta", "at"]),
     ]
     for inputs, outputs in test_pairs:
-        print('Inputs:', inputs)
-        print('Outputs:', outputs)
+        print("------------------")
+        print("Inputs:", inputs)
+        print("Outputs:", outputs)
         synth = Synthesizer(inputs, outputs, StringSyntaxTree)
         result = synth.synthesize()
-        print('Program:', result)
+        print("Program:", result)
+
 
 if __name__ == "__main__":
     test_arithmetic()
